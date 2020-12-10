a4ab569b51e92d8d8f65e476128e812d7c27b161
Chris <firstof9@gmail.com>
2020-12-10 02:20:10 -0700

    Initial unit tests for config flow (#9)
    * Add unit tests for config flow
    
    * adjust order of options test

---
+++ tests/const.py
@@ -0,0 +1,13 @@
+""" Constants for tests. """
+
+CONFIG_DATA = {
+    "alarm_level": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_level_frontdoor",
+    "alarm_type": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_type_frontdoor",
+    "entity_id": "lock.kwikset_touchpad_electronic_deadbolt_frontdoor",
+    "lockname": "frontdoor",
+    "using_ozw": True,
+    "packages_path": "packages/keymaster",
+    "sensorname": "binary_sensor.frontdoor",
+    "slots": 6,
+    "start_from": 1,
+}
