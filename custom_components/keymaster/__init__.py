d80fbad20590b512e9f03d278b091cb6ea50922d
Raman Gupta <7243222+raman325@users.noreply.github.com>
2020-12-28 17:37:37 -0500

    clear unsubscription callback list after unsubscribing

--- custom_components/keymaster/__init__.py
+++ custom_components/keymaster/__init__.py
@@ -252,6 +252,7 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> 
         # Unsubscribe to any listeners
         for unsub_listener in hass.data[DOMAIN].get(UNSUB_LISTENERS, []):
             unsub_listener()
+        hass.data[DOMAIN].get(UNSUB_LISTENERS, []).clear()
 
         hass.data[DOMAIN].pop(config_entry.entry_id)
 
@@ -352,6 +353,7 @@ async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> Non
     # Unsubscribe to any listeners so we can create new ones
     for unsub_listener in hass.data[DOMAIN].get(UNSUB_LISTENERS, []):
         unsub_listener()
+    hass.data[DOMAIN].get(UNSUB_LISTENERS, []).clear()
 
     # Create new listeners
     hass.data[DOMAIN][config_entry.entry_id][UNSUB_LISTENERS].append(
