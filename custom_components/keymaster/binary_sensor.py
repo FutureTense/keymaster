1945f78816ae7dbd66853963c59e9d2d613d6459
raman325 <7243222+raman325@users.noreply.github.com>
2021-01-22 17:17:01 -0500

    move synchronize code slot automation logic into binary sensor

--- custom_components/keymaster/binary_sensor.py
+++ custom_components/keymaster/binary_sensor.py
@@ -1,7 +1,9 @@
 """Binary sensors for keymaster."""
 from datetime import datetime
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, Optional
+
+from openzwavemqtt.const import ATTR_CODE_SLOT
 
 from homeassistant.components.binary_sensor import (
     DOMAIN as BINARY_SENSOR_DOMAIN,
@@ -13,16 +15,24 @@ from homeassistant.components.input_number import DOMAIN as INPUT_NUM_DOMAIN
 from homeassistant.components.input_text import DOMAIN as INPUT_TXT_DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_FRIENDLY_NAME, ATTR_NOW
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    ATTR_FRIENDLY_NAME,
+    ATTR_NOW,
+    STATE_OFF,
+    STATE_ON,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.event import (
     Event,
+    async_track_state_change,
     async_track_state_change_event,
     async_track_time_change,
 )
 from homeassistant.util import dt
 
-from .const import CONF_SLOTS, CONF_START
+from . import SERVICE_ADD_CODE, SERVICE_CLEAR_CODE
+from .const import ATTR_USER_CODE, CONF_SLOTS, CONF_START, DOMAIN
 from .entity import KeymasterTemplateEntity
 
 _LOGGER = logging.getLogger(__name__)
@@ -57,6 +67,10 @@ class PinSynchedSensor(BinarySensorEntity, KeymasterTemplateEntity):
             "PIN Synched",
             "PIN synchronized with lock",
         )
+        self._automation_allowed_unsub = None
+        self._automation_allowed_entity = (
+            "input_boolean.keymaster_allow_automation_execution"
+        )
         self._lock_pin_entity = self.get_entity_id(SENSOR_DOMAIN, "code_slot")
         self._input_pin_entity = self.get_entity_id(INPUT_TXT_DOMAIN, "pin")
         self._active_entity = self.get_entity_id(BINARY_SENSOR_DOMAIN, "active")
@@ -83,10 +97,40 @@ class PinSynchedSensor(BinarySensorEntity, KeymasterTemplateEntity):
             and ((active and input_pin == lock_pin) or (not active and lock_pin == ""))
         )
 
+    async def call_code_slot_service(self, active: Optional[str] = None) -> None:
+        """Call appropriate code slot service based on active state."""
+        if not active:
+            active = self.get_state(self._active_entity)
+
+        if active == STATE_OFF:
+            await self.hass.services.async_call(
+                DOMAIN,
+                SERVICE_CLEAR_CODE,
+                {
+                    ATTR_ENTITY_ID: self._lock.lock_entity_id,
+                    ATTR_CODE_SLOT: self._code_slot,
+                },
+            )
+        elif active == STATE_ON:
+            await self.hass.services.async_call(
+                DOMAIN,
+                SERVICE_ADD_CODE,
+                {
+                    ATTR_ENTITY_ID: self._lock.lock_entity_id,
+                    ATTR_CODE_SLOT: self._code_slot,
+                    ATTR_USER_CODE: self.get_state(self._input_pin_entity),
+                },
+            )
+
     async def async_added_to_hass(self) -> None:
         """Run when entity about to be added to hass."""
 
-        def state_change_handler(evt: Event) -> None:
+        async def automation_allowed_handler() -> None:
+            """Handle state change event when automation is allowed."""
+            self._automation_allowed_unsub()
+            self.call_code_slot_service()
+
+        async def state_change_handler(evt: Event) -> None:
             """Handle state change events for watched entities."""
             if evt:
                 _LOGGER.debug(
@@ -103,6 +147,23 @@ class PinSynchedSensor(BinarySensorEntity, KeymasterTemplateEntity):
                 ],
             )
             self.async_write_ha_state()
+
+            # Handle active entity state change by setting or clearing code
+            if evt.data.get("entity_id") == self._active_entity:
+                active = evt.data["new_state"].state
+                # if automation isn't allowed yet, set up execution for later
+                if (
+                    self.get_state("input_boolean.keymaster_allow_automation_execution")
+                    == STATE_ON
+                ):
+                    await self.call_code_slot_service(active)
+                else:
+                    self._automation_allowed_unsub = async_track_state_change(
+                        self._hass,
+                        self._automation_allowed_entity,
+                        automation_allowed_handler,
+                        to_state=STATE_ON,
+                    )
 
         self.async_on_remove(
             async_track_state_change_event(
