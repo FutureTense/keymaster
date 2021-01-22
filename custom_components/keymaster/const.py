8686682d4230b24ef5202a887637c62558c003d4
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-01-21 20:08:53 -0600

    Add more debug logs (#41)
    * add more debug logs
    
    * more typehints

--- custom_components/keymaster/const.py
+++ custom_components/keymaster/const.py
@@ -57,6 +57,29 @@ DEFAULT_GENERATE = True
 DEFAULT_DOOR_SENSOR = "binary_sensor.fake"
 DEFAULT_HIDE_PINS = False
 
+# Connected sensor maps
+CONNECTED_STATE_MAP = {
+    True: {
+        True: "Connected",
+        False: "Connecting",
+    },
+    False: {
+        True: "Disconnected",
+        False: "Disconnecting",
+    },
+}
+
+CONNECTED_ICON_MAP = {
+    True: {
+        True: "mdi:folder-key",
+        False: "mdi:folder-key-network",
+    },
+    False: {
+        True: "mdi:folder-open",
+        False: "mdi:wiper-watch",
+    },
+}
+
 # Action maps
 ACTION_MAP = {
     ALARM_TYPE: {
--- custom_components/keymaster/sensor.py
+++ custom_components/keymaster/sensor.py
@@ -1,5 +1,6 @@
 """Sensors for keymaster."""
 import logging
+from typing import Dict, Optional
 
 from openzwavemqtt.const import ATTR_CODE_SLOT
 
@@ -10,7 +11,14 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.event import Event, async_track_state_change_event
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import CONF_SLOTS, CONF_START, COORDINATOR, DOMAIN
+from .const import (
+    CONF_SLOTS,
+    CONF_START,
+    CONNECTED_ICON_MAP,
+    CONNECTED_STATE_MAP,
+    COORDINATOR,
+    DOMAIN,
+)
 from .entity import KeymasterTemplateEntity
 
 _LOGGER = logging.getLogger(__name__)
@@ -28,13 +36,13 @@ async def async_setup_entry(
         ConnectedSensor(hass, entry, x)
         for x in range(entry.data[CONF_START], entry.data[CONF_SLOTS] + 1)
     ]
-    async_add_entities(sensors, True)
+    async_add_entities(sensors)
 
 
 class CodesSensor(CoordinatorEntity, KeymasterTemplateEntity):
     """Sensor class for code slot PINs."""
 
-    def __init__(self, hass: HomeAssistant, entry: ConfigEntry, code_slot: int):
+    def __init__(self, hass: HomeAssistant, entry: ConfigEntry, code_slot: int) -> None:
         """Initialize the sensor."""
         KeymasterTemplateEntity.__init__(
             self, hass, entry, SENSOR_DOMAIN, code_slot, "Code Slot"
@@ -42,7 +50,7 @@ class CodesSensor(CoordinatorEntity, KeymasterTemplateEntity):
         CoordinatorEntity.__init__(self, hass.data[DOMAIN][entry.entry_id][COORDINATOR])
 
     @property
-    def state(self):
+    def state(self) -> Optional[str]:
         """Return the state of the sensor."""
         try:
             return self.coordinator.data.get(self._code_slot)
@@ -52,17 +60,17 @@ class CodesSensor(CoordinatorEntity, KeymasterTemplateEntity):
             )
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the entity name."""
         return f"{self._lock_name} {self._name} {self._code_slot}"
 
     @property
-    def icon(self):
+    def icon(self) -> str:
         """Return the icon."""
         return "mdi:lock-smart"
 
     @property
-    def device_state_attributes(self):
+    def device_state_attributes(self) -> Dict[str, int]:
         """Return device specific state attributes."""
         return {ATTR_CODE_SLOT: self._code_slot}
 
@@ -70,7 +78,7 @@ class CodesSensor(CoordinatorEntity, KeymasterTemplateEntity):
 class ConnectedSensor(KeymasterTemplateEntity):
     """Sensor class for code slot connections."""
 
-    def __init__(self, hass: HomeAssistant, entry: ConfigEntry, code_slot: int):
+    def __init__(self, hass: HomeAssistant, entry: ConfigEntry, code_slot: int) -> None:
         """Initialize the sensor."""
         KeymasterTemplateEntity.__init__(
             self, hass, entry, SENSOR_DOMAIN, code_slot, "Connected", "Status"
@@ -94,35 +102,22 @@ class ConnectedSensor(KeymasterTemplateEntity):
         )
 
     @property
-    def state(self):
+    def state(self) -> str:
         """Return the state of the sensor."""
-        map = {
-            True: {
-                True: "Connected",
-                False: "Connecting",
-            },
-            False: {
-                True: "Disconnected",
-                False: "Disconnecting",
-            },
-        }
         active = self.get_state(self._active_entity)
         pin_synched = self.get_state(self._pin_synched_entity)
-        return map[active][pin_synched]
+        state = CONNECTED_STATE_MAP[active][pin_synched]
+
+        _LOGGER.debug("Updating state for %s...", self.entity_id)
+        _LOGGER.debug("Input state for %s is %s", self._active_entity, active)
+        _LOGGER.debug("Input state for %s is %s", self._pin_synched_entity, pin_synched)
+        _LOGGER.debug("Output state for %s is %s", self.entity_id, state)
+
+        return state
 
     @property
-    def icon(self):
+    def icon(self) -> str:
         """Return the icon."""
-        map = {
-            True: {
-                True: "mdi:folder-key",
-                False: "mdi:folder-key-network",
-            },
-            False: {
-                True: "mdi:folder-open",
-                False: "mdi:wiper-watch",
-            },
-        }
         active = self.get_state(self._active_entity)
         pin_synched = self.get_state(self._pin_synched_entity)
-        return map[active][pin_synched]
+        return CONNECTED_ICON_MAP[active][pin_synched]
