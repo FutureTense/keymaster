9db45c6dd200cc36489981d6a4893be67783896c
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-02-05 11:53:05 -0600

    remove Node typehint since it creates problems (#75)

--- custom_components/keymaster/lock.py
+++ custom_components/keymaster/lock.py
@@ -4,14 +4,6 @@ from typing import Optional
 
 from homeassistant.helpers.device_registry import DeviceEntry
 
-# TODO: At some point we should assume that users have upgraded to the latest
-# Home Assistant instance and that we can safely import these, so we can move
-# these back to standard imports at that point.
-try:
-    from zwave_js_server.model.node import Node
-except (ModuleNotFoundError, ImportError):
-    from openzwavemqtt.models.node import Node
-
 
 @dataclass
 class KeymasterLock:
@@ -22,5 +14,5 @@ class KeymasterLock:
     alarm_level_or_user_code_entity_id: Optional[str]
     alarm_type_or_access_control_entity_id: Optional[str]
     door_sensor_entity_id: Optional[str]
-    zwave_js_lock_node: Node = None
+    zwave_js_lock_node = None
     zwave_js_lock_device: DeviceEntry = None
