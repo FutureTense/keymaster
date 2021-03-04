953957c04f3a992971fcd8d4e8b718009d01e5a1
firstof9 <firstof9@gmail.com>
2021-03-04 09:45:44 -0700

    Update test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,10 +2,16 @@
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.keymaster.const import DOMAIN
+from custom_components.keymaster.helpers import using_ozw
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant import setup
 
+from homeassistant.const import EVENT_HOMEASSISTANT_STARTED
+
 from tests.const import CONFIG_DATA, CONFIG_DATA_OLD, CONFIG_DATA_REAL
+from .common import setup_ozw
+
+NETWORK_READY_ENTITY = "binary_sensor.keymaster_zwave_network_ready"
 
 
 async def test_setup_entry(hass, mock_generate_package_files):
@@ -68,3 +74,31 @@ async def test_setup_migration_with_old_path(hass, mock_generate_package_files):
     assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 6
     entries = hass.config_entries.async_entries(DOMAIN)
     assert len(entries) == 1
+
+
+async def test_update_usercodes_using_ozw(hass, lock_data):
+    """Test handle_state_change"""
+
+    await setup_ozw(hass, fixture=lock_data)
+
+    assert "ozw" in hass.config.components
+
+    # Load the integration
+    entry = MockConfigEntry(
+        domain=DOMAIN, title="frontdoor", data=CONFIG_DATA_REAL, version=2
+    )
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    # Fire the event
+    hass.bus.async_fire(EVENT_HOMEASSISTANT_STARTED)
+    await hass.async_block_till_done()
+
+    assert using_ozw(hass)
+
+    # TODO: Find a way to turn on the binary_sensor for ozw
+    assert hass.states.get(NETWORK_READY_ENTITY)
+    # assert hass.states.get(NETWORK_READY_ENTITY).state == "on"
+
+    # assert hass.states.get("sensor.frontdoor_code_slot_1") == "12345678"
