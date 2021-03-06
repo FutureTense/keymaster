dca26c1594be852f8ef4d08ba6e1ba8f915d7ce0
firstof9 <firstof9@gmail.com>
2021-03-06 10:31:32 -0700

    Update test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -115,8 +115,6 @@ async def test_update_usercodes_using_zwave(hass, mock_openzwave, caplog):
     hass.bus.async_fire(EVENT_HOMEASSISTANT_STARTED)
     await hass.async_block_till_done()
 
-    assert "Z-Wave integration not found" not in caplog.text
-
     assert hass.states.get(NETWORK_READY_ENTITY)
     hass.states.async_set(NETWORK_READY_ENTITY, "on")
     await hass.async_block_till_done()
