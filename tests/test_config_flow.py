43644c8046cb78b6cbb8de3ace6e18fa8f30dc5d
Chris <firstof9@gmail.com>
2021-03-08 19:13:04 -0700

    Additional init and config_flow tests (#127)
    * Additional init and config_flow tests
    
    * Update .coveragerc

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -19,6 +19,7 @@ from .common import setup_ozw
 
 from tests.const import CONFIG_DATA
 
+KWIKSET_910_LOCK_ENTITY = "lock.smart_code_with_home_connect_technology"
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -186,16 +187,6 @@ async def test_form_invalid_path(input_1, title, data, mock_get_entities, hass):
         assert result2["errors"] == {CONF_PATH: "invalid_path"}
 
 
-# async def test_valid_path():
-#     result = await _validate_path(os.path.dirname(__file__))
-#     assert result
-
-
-# async def test_invalid_path():
-#     result = await _validate_path("/should/fail")
-#     assert not result
-
-
 @pytest.mark.parametrize(
     "input_1,title,data",
     [
@@ -227,6 +218,145 @@ async def test_form_invalid_path(input_1, title, data, mock_get_entities, hass):
 )
 async def test_options_flow(input_1, title, data, hass, mock_get_entities):
     """Test config flow options."""
+    _LOGGER.error(_get_schema(hass, CONFIG_DATA, KeyMasterFlowHandler.DEFAULTS))
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="frontdoor",
+        data=_get_schema(hass, CONFIG_DATA, KeyMasterFlowHandler.DEFAULTS)(CONFIG_DATA),
+        version=2,
+    )
+
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+
+    assert result["type"] == "form"
+    assert result["step_id"] == "init"
+    assert result["errors"] == {}
+
+    with patch(
+        "custom_components.keymaster.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "custom_components.keymaster.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"], input_1
+        )
+        assert result2["type"] == "create_entry"
+
+        await hass.async_block_till_done()
+        assert entry.data.copy() == data
+
+
+@pytest.mark.parametrize(
+    "input_1,title,data",
+    [
+        (
+            {
+                "alarm_level_or_user_code_entity_id": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_level_frontdoor",
+                "alarm_type_or_access_control_entity_id": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_type_frontdoor",
+                "lock_entity_id": "lock.kwikset_touchpad_electronic_deadbolt_frontdoor",
+                "lockname": "sidedoor",
+                "packages_path": "packages/keymaster_test",
+                "sensorname": "binary_sensor.frontdoor",
+                "slots": 4,
+                "start_from": 1,
+            },
+            "frontdoor",
+            {
+                "alarm_level_or_user_code_entity_id": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_level_frontdoor",
+                "alarm_type_or_access_control_entity_id": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_type_frontdoor",
+                "lock_entity_id": "lock.kwikset_touchpad_electronic_deadbolt_frontdoor",
+                "lockname": "sidedoor",
+                "packages_path": "packages/keymaster_test",
+                "sensorname": "binary_sensor.frontdoor",
+                "slots": 4,
+                "start_from": 1,
+                "hide_pins": False,
+            },
+        ),
+    ],
+)
+async def test_options_flow_path_change(input_1, title, data, hass, mock_get_entities):
+    """Test config flow options."""
+    _LOGGER.error(_get_schema(hass, CONFIG_DATA, KeyMasterFlowHandler.DEFAULTS))
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="frontdoor",
+        data=_get_schema(hass, CONFIG_DATA, KeyMasterFlowHandler.DEFAULTS)(CONFIG_DATA),
+        version=2,
+    )
+
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+
+    assert result["type"] == "form"
+    assert result["step_id"] == "init"
+    assert result["errors"] == {}
+
+    with patch(
+        "custom_components.keymaster.async_setup", return_value=True
+    ) as mock_setup, patch(
+        "custom_components.keymaster.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+
+        result2 = await hass.config_entries.options.async_configure(
+            result["flow_id"], input_1
+        )
+        assert result2["type"] == "create_entry"
+
+        await hass.async_block_till_done()
+        assert entry.data.copy() == data
+
+
+@pytest.mark.parametrize(
+    "input_1,title,data",
+    [
+        (
+            {
+                "alarm_level_or_user_code_entity_id": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_level_frontdoor",
+                "alarm_type_or_access_control_entity_id": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_type_frontdoor",
+                "lock_entity_id": "lock.kwikset_touchpad_electronic_deadbolt_frontdoor",
+                "lockname": "sidedoor",
+                "packages_path": "packages/keymaster",
+                "sensorname": "binary_sensor.frontdoor",
+                "slots": 4,
+                "start_from": 1,
+            },
+            "frontdoor",
+            {
+                "alarm_level_or_user_code_entity_id": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_level_frontdoor",
+                "alarm_type_or_access_control_entity_id": "sensor.kwikset_touchpad_electronic_deadbolt_alarm_type_frontdoor",
+                "lock_entity_id": "lock.kwikset_touchpad_electronic_deadbolt_frontdoor",
+                "lockname": "sidedoor",
+                "packages_path": "packages/keymaster",
+                "sensorname": "binary_sensor.frontdoor",
+                "slots": 4,
+                "start_from": 1,
+                "hide_pins": False,
+            },
+        ),
+    ],
+)
+async def test_options_flow_with_zwavejs(
+    input_1, title, data, hass, mock_get_entities, client, lock_kwikset_910, integration
+):
+    """Test config flow options."""
+
+    # Load ZwaveJS
+    node = lock_kwikset_910
+    state = hass.states.get(KWIKSET_910_LOCK_ENTITY)
+
     _LOGGER.error(_get_schema(hass, CONFIG_DATA, KeyMasterFlowHandler.DEFAULTS))
     entry = MockConfigEntry(
         domain=DOMAIN,
