35ebe2aba05548e99923bad21c0b08a364908a6e
Chris <firstof9@gmail.com>
2022-05-27 13:03:21 -0700

    update test

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -27,11 +27,7 @@ SCHLAGE_BE469_LOCK_ENTITY = "lock.touchscreen_deadbolt_current_lock_mode"
 KWIKSET_910_LOCK_ENTITY = "lock.smart_code_with_home_connect_technology"
 
 
-async def test_delete_lock_and_base_folder(
-    hass,
-    mock_osremove,
-    mock_osrmdir,
-):
+async def test_delete_lock_and_base_folder(hass):
     """Test delete_lock_and_base_folder"""
     entry = MockConfigEntry(
         domain=DOMAIN, title="frontdoor", data=CONFIG_DATA, version=2
@@ -40,13 +36,13 @@ async def test_delete_lock_and_base_folder(
     entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
-
-    delete_lock_and_base_folder(hass, entry)
-
-    assert mock_osrmdir.called
-    assert mock_osremove.called
-
+    
     with patch("custom_components.keymaster.helpers.os", autospec=True) as mock_os:
+        delete_lock_and_base_folder(hass, entry)
+
+        assert mock_os.rmdir.called
+        assert mock_os.remove.called
+    
         mock_os.listdir.return_value = False
         delete_lock_and_base_folder(hass, entry)
         mock_os.rmdir.assert_called_once
