65b6ba06f6a757dd3d0b0c700cfe9aab318769f2
Snuffy2 <Snuffy2@users.noreply.github.com>
2024-11-19 09:20:09 -0500

    Update Listeners

--- custom_components/keymaster/helpers.py
+++ custom_components/keymaster/helpers.py
@@ -2,11 +2,13 @@
 
 import asyncio
 from collections.abc import Mapping
-from datetime import timedelta
-import functools
 import logging
+import time
 from typing import Any
 
+from zwave_js_server.const.command_class.lock import ATTR_CODE_SLOT
+
+from homeassistant.components import persistent_notification
 from homeassistant.components.automation import DOMAIN as AUTO_DOMAIN
 from homeassistant.components.input_boolean import DOMAIN as IN_BOOL_DOMAIN
 from homeassistant.components.input_datetime import DOMAIN as IN_DT_DOMAIN
@@ -15,54 +17,36 @@ from homeassistant.components.input_text import DOMAIN as IN_TXT_DOMAIN
 from homeassistant.components.script import DOMAIN as SCRIPT_DOMAIN
 from homeassistant.components.template import DOMAIN as TEMPLATE_DOMAIN
 from homeassistant.components.timer import DOMAIN as TIMER_DOMAIN
-from homeassistant.const import (
-    ATTR_DEVICE_ID,
-    ATTR_ENTITY_ID,
-    ATTR_STATE,
-    SERVICE_RELOAD,
-    STATE_UNAVAILABLE,
-    STATE_UNKNOWN,
-)
-from homeassistant.core import Event, EventStateChangedData, HomeAssistant, callback
+from homeassistant.components.zwave_js.const import DOMAIN as ZWAVE_JS_DOMAIN
+from homeassistant.const import SERVICE_RELOAD, STATE_UNKNOWN
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import ServiceNotFound
 from homeassistant.helpers import entity_registry as er
-from homeassistant.helpers.event import async_track_state_change_event
-from homeassistant.util import dt as dt_util
 
-from .const import (
-    ACCESS_CONTROL,
-    ACTION_MAP,
-    ALARM_TYPE,
-    ATTR_ACTION_CODE,
-    ATTR_ACTION_TEXT,
-    ATTR_CODE_SLOT_NAME,
-    ATTR_NAME,
-    ATTR_NOTIFICATION_SOURCE,
-    CONF_SLOTS,
-    CONF_START,
-    EVENT_KEYMASTER_LOCK_STATE_CHANGED,
-    LOCK_STATE_MAP,
-)
+from .const import CONF_SLOTS, CONF_START
 from .lock import KeymasterLock
 
+ATTR_CODE_SLOT = "code_slot"
 zwave_js_supported = True
+_LOGGER: logging.Logger = logging.getLogger(__name__)
 
-try:
-    from zwave_js_server.const.command_class.lock import ATTR_CODE_SLOT
 
-    from homeassistant.components.zwave_js.const import (
-        ATTR_EVENT_LABEL,
-        ATTR_NODE_ID,
-        ATTR_PARAMETERS,
-        DATA_CLIENT as ZWAVE_JS_DATA_CLIENT,
-        DOMAIN as ZWAVE_JS_DOMAIN,
-    )
-except (ModuleNotFoundError, ImportError):
-    zwave_js_supported = False
-    ATTR_CODE_SLOT = "code_slot"
-    from .const import ATTR_NODE_ID
+class Throttle:
+    def __init__(self):
+        self.cooldowns = (
+            {}
+        )  # Nested dictionary: {function_name: {key: last_called_time}}
 
-_LOGGER = logging.getLogger(__name__)
+    def is_allowed(self, func_name, key, cooldown_seconds):
+        current_time = time.time()
+        if func_name not in self.cooldowns:
+            self.cooldowns[func_name] = {}
+
+        last_called = self.cooldowns[func_name].get(key, 0)
+        if current_time - last_called >= cooldown_seconds:
+            self.cooldowns[func_name][key] = current_time
+            return True
+        return False
 
 
 @callback
@@ -143,159 +127,6 @@ def get_code_slots_list(data: Mapping[str, int]) -> list[int]:
 #         for file_or_dir in os.listdir(path):
 #             delete_folder(path, file_or_dir)
 #         os.rmdir(path)
-
-
-def handle_zwave_js_event(
-    hass: HomeAssistant, kmlock: KeymasterLock, evt: Event
-) -> None:
-    """Handle Z-Wave JS event."""
-    if (
-        not kmlock.zwave_js_lock_node
-        or not kmlock.zwave_js_lock_device
-        or evt.data[ATTR_NODE_ID] != kmlock.zwave_js_lock_node.node_id
-        or evt.data[ATTR_DEVICE_ID] != kmlock.zwave_js_lock_device.id
-    ):
-        return
-
-    # Get lock state to provide as part of event data
-    lock_state = hass.states.get(kmlock.lock_entity_id)
-
-    params = evt.data.get(ATTR_PARAMETERS) or {}
-    code_slot = params.get("userId", 0)
-
-    # Lookup name for usercode
-    code_slot_name_state = (
-        hass.states.get(f"input_text.{kmlock.lock_name}_name_{code_slot}")
-        if code_slot and code_slot != 0
-        else None
-    )
-
-    hass.bus.fire(
-        EVENT_KEYMASTER_LOCK_STATE_CHANGED,
-        event_data={
-            ATTR_NOTIFICATION_SOURCE: "event",
-            ATTR_NAME: kmlock.lock_name,
-            ATTR_ENTITY_ID: kmlock.lock_entity_id,
-            ATTR_STATE: lock_state.state if lock_state else "",
-            ATTR_ACTION_TEXT: evt.data.get(ATTR_EVENT_LABEL),
-            ATTR_CODE_SLOT: code_slot or 0,
-            ATTR_CODE_SLOT_NAME: (
-                code_slot_name_state.state if code_slot_name_state is not None else ""
-            ),
-        },
-    )
-    return
-
-
-async def homeassistant_started_listener(
-    hass: HomeAssistant,
-    kmlock: KeymasterLock,
-    evt: Event = None,
-):
-    """Start tracking state changes after HomeAssistant has started."""
-    # Listen to lock state changes so we can fire an event
-    kmlock.listeners.append(
-        async_track_state_change_event(
-            hass,
-            kmlock.lock_entity_id,
-            functools.partial(handle_state_change, hass, kmlock),
-        )
-    )
-
-
-@callback
-def handle_state_change(
-    hass: HomeAssistant,
-    kmlock: KeymasterLock,
-    changed_entity: str,
-    event: Event[EventStateChangedData] | None = None,
-) -> None:
-    """Listener to track state changes to lock entities."""
-    if not event:
-        return
-
-    new_state = event.data["new_state"]
-
-    # Don't do anything if the changed entity is not this lock
-    if changed_entity != kmlock.lock_entity_id:
-        return
-
-    # Determine action type to set appropriate action text using ACTION_MAP
-    action_type = ""
-    if kmlock.alarm_type_or_access_control_entity_id and (
-        ALARM_TYPE in kmlock.alarm_type_or_access_control_entity_id
-        or ALARM_TYPE.replace("_", "") in kmlock.alarm_type_or_access_control_entity_id
-    ):
-        action_type = ALARM_TYPE
-    if (
-        kmlock.alarm_type_or_access_control_entity_id
-        and ACCESS_CONTROL in kmlock.alarm_type_or_access_control_entity_id
-    ):
-        action_type = ACCESS_CONTROL
-
-    # Get alarm_level/usercode and alarm_type/access_control  states
-    alarm_level_state = hass.states.get(kmlock.alarm_level_or_user_code_entity_id)
-    alarm_level_value = (
-        int(alarm_level_state.state)
-        if alarm_level_state
-        and alarm_level_state.state not in (STATE_UNKNOWN, STATE_UNAVAILABLE)
-        else None
-    )
-
-    alarm_type_state = hass.states.get(kmlock.alarm_type_or_access_control_entity_id)
-    alarm_type_value = (
-        int(alarm_type_state.state)
-        if alarm_type_state
-        and alarm_type_state.state not in (STATE_UNKNOWN, STATE_UNAVAILABLE)
-        else None
-    )
-
-    # Bail out if we can't use the sensors to provide a meaningful message
-    if alarm_level_value is None or alarm_type_value is None:
-        return
-
-    # If lock has changed state but alarm_type/access_control state hasn't changed
-    # in a while set action_value to RF lock/unlock
-    if (
-        alarm_level_state is not None
-        and int(alarm_level_state.state) == 0
-        and dt_util.utcnow() - dt_util.as_utc(alarm_type_state.last_changed)
-        > timedelta(seconds=5)
-        and action_type in LOCK_STATE_MAP
-    ):
-        alarm_type_value = LOCK_STATE_MAP[action_type][new_state.state]
-
-    # Lookup action text based on alarm type value
-    action_text = (
-        ACTION_MAP.get(action_type, {}).get(
-            alarm_type_value, "Unknown Alarm Type Value"
-        )
-        if alarm_type_value is not None
-        else None
-    )
-
-    # Lookup name for usercode
-    code_slot_name_state = hass.states.get(
-        f"input_text.{kmlock.lock_name}_name_{alarm_level_value}"
-    )
-
-    # Fire state change event
-    hass.bus.fire(
-        EVENT_KEYMASTER_LOCK_STATE_CHANGED,
-        event_data={
-            ATTR_NOTIFICATION_SOURCE: "entity_state",
-            ATTR_NAME: kmlock.lock_name,
-            ATTR_ENTITY_ID: kmlock.lock_entity_id,
-            ATTR_STATE: new_state.state,
-            ATTR_ACTION_CODE: alarm_type_value,
-            ATTR_ACTION_TEXT: action_text,
-            ATTR_CODE_SLOT: alarm_level_value or 0,
-            ATTR_CODE_SLOT_NAME: (
-                code_slot_name_state.state if code_slot_name_state is not None else ""
-            ),
-        },
-    )
-    return
 
 
 def reset_code_slot_if_pin_unknown(
@@ -379,3 +210,17 @@ async def call_hass_service(
             str(e),
         )
         # raise e
+
+
+async def send_persistent_notification(
+    hass: HomeAssistant, message: str, title: str = None, notification_id: str = None
+) -> None:
+    persistent_notification.async_create(
+        hass=hass, message=message, title=title, notification_id=notification_id
+    )
+
+
+async def dismiss_persistent_notification(
+    hass: HomeAssistant, notification_id: str
+) -> None:
+    persistent_notification.async_dismiss(hass=hass, notification_id=notification_id)
