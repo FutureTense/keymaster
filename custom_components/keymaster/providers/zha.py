3beef5be16f6677c24b4ffe3edb0171c871fa1e8
firstof9@gmail.com <firstof9@gmail.com>
2026-06-12 13:00:45 -0700

    Safely import zigpy in zha.py and add zigpy to requirements_test.txt

--- custom_components/keymaster/providers/zha.py
+++ custom_components/keymaster/providers/zha.py
@@ -8,16 +8,16 @@ from dataclasses import dataclass, field
 import logging
 from typing import TYPE_CHECKING, Any
 
-from zigpy.zcl.clusters.closures import DoorLock
-
-from custom_components.keymaster.const import CONF_SLOTS, CONF_START, COORDINATOR, DOMAIN
-
 try:
+    from zigpy.zcl.clusters.closures import DoorLock
+
     from homeassistant.components.zha.const import DOMAIN as ZHA_DOMAIN
     from homeassistant.components.zha.helpers import get_zha_gateway_proxy
 except ImportError:
+    DoorLock = None  # type: ignore[assignment, unused-ignore]
     ZHA_DOMAIN = "zha"
     get_zha_gateway_proxy = None
+from custom_components.keymaster.const import CONF_SLOTS, CONF_START, COORDINATOR, DOMAIN
 from homeassistant.const import STATE_UNAVAILABLE, STATE_UNKNOWN
 from homeassistant.core import Event, callback as ha_callback
 from homeassistant.helpers.event import async_track_state_change_event
